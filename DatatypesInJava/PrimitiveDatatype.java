74ad29d13e122edbd0475998e9c3c24af651c5ca
sagartmohite <sagarmohite.test@gmail.com>
2024-04-23 13:03:07 +0530

    first commit

---
+++ DatatypesInJava/PrimitiveDatatype.java
@@ -0,0 +1,30 @@
+package DatatypesInJava;
+
+public class PrimitiveDatatype{
+    public static void main(String[] args) {
+        //PrimitiveDatatype--> 1.Numeric-->1.Integral--> Byte,Short,Int,Long,Char
+        //                                  2.Floating --> Float, Double
+        //                      2.Boolean
+
+        Byte b=11;
+        System.out.println(b);
+        Short s = 12;
+        System.out.println(s);
+        int i = 13;
+        System.out.println(i);
+        long l = 15;
+        System.out.println(l);
+        char  c = 'A';
+        System.out.println(c);
+
+        float f = 45.45f;
+        System.out.println(f);
+        double d = 12.12;
+        System.out.println(d);
+
+        boolean b1 = true;
+        System.out.println(b1);
+        b1 = false;
+        System.out.println(b1);
+    }
+}
---
+++ Variables/Demo.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class Demo {
+    public static void main(String[] args) {
+        //PRIMITIVE DATATYPE
+        int result = 100;
+        System.out.println(result);
+        int $ = 34;
+        System.out.println($);
+        int i = 100;
+        System.out.println(i);
+        //int 123 = 101;
+        //int 123_sager = 102;
+        //int 1name =103;
+        System.out.println($);
+        // REFERENCE DATATYPE
+        java.lang.String str = "sager";
+        System.out.println(str);
+    }
+}
